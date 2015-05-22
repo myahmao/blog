6004ad2eb839a662bbdf8e4b32e5bef68c2bf7eb
yong <ahyong@gmail.com>
2015-05-22 14:10:11 -0700

    add all files to repo

---
+++ content/first.md
@@ -0,0 +1,9 @@
+Title: 第一篇
+Date: 5/22/2015
+Modified: 5/22/2015
+Category: new
+Tags: 
+Slug: first
+Author: yong
+
+第一篇，哈哈
---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'ahmao'
+SITENAME = u'\u4e0d\u6094\uff0c\u5c31\u662f\u4e0d\u6094'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'zh'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'))
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Use bootstrap3 theme
+THEME = 'themes/pelican-bootstrap3'
+#FAVICON = 'images/favicon.ico'
+BOOTSTRAP_THEME = 'cosmo'
+DISPLAY_TAGS_ON_SIDEBAR = True
+DISPLAY_CATEGORIES_ON_SIDEBAR = True
+DISPLAY_TAGS_ON_SIDEBAR = True
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
